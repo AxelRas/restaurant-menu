2d0293c5cd0344a0046b9ba01e5f7c306591d2c2
Axel Rasmussen <axl.rasmussen@gmail.com>
2021-03-29 10:39:16 -0300

    Added colors and dynamic position

--- src/components/Items.js
+++ src/components/Items.js
@@ -5,12 +5,12 @@ export default class Items extends Component {
         return (
             <div className="items">
                 {this.props.data.map(item => (
-                    <div className="item">
-                        <img src={item.image}></img>
+                    <div key={item.image} className="item">
+                        <img src={item.image} alt={item.image}></img>
                         <div className="item-text">
                             <div className="item-title-price">
                                 <p className="item-title">{item.name}</p>
-                                <p className="item-price">{item.price}</p>
+                                <p className="item-price">{"$"+item.price}</p>
                             </div>
                             <p className="item-desc">{item.desc}</p>
                         </div>
